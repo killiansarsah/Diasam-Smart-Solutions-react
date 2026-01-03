9ea8db490a4ddd06f534c71a3123b9434728a61b
killian sarsah <killiansarsah@gmail.com>
2026-01-03 02:36:25 +0000

    fix: Update contact email address in index.html and contact-mailer.php

--- vendor/contact-mailer.php
+++ vendor/contact-mailer.php
@@ -14,7 +14,7 @@ if($_POST)
 
     $mail = new PHPMailer();
 
-    $your_email = "youremail@website.com";
+    $your_email = "info@diasamsolutions.com";
 
 
     //check if its an ajax request, exit if not
